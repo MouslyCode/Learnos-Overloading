3757b6280f167c1d6a61d94f470eb2db6de8805a
MouslyCode <rezasubkhan50@gmail.com>
2022-07-27 07:40:26 +0700

    Pertamax Commit

---
+++ src/overheaven/Matematika.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package overheaven;
+
+/**
+ *
+ * @author ASUS A455L
+ */
+public class Matematika {
+    
+    
+    public void hitung(int a, int b, int c){
+        System.out.println("Campuran : "+c*(a+b));
+    
+    }
+ public void hitung(int a, double b){
+        System.out.println("Perkalian : "+(a*b));
+ }
+  public void hitung(int a, int d){
+        System.out.println("Pengurangan : "+(a-d));
+  }
+}
---
+++ src/overheaven/OverHeaven.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package overheaven;
+
+/**
+ *
+ * @author ASUS A455L
+ */
+public class OverHeaven {
+
+    /**o
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+Matematika angka = new Matematika ();
+
+angka.hitung(9, 10, 15);
+angka.hitung(99, 5.0);
+angka.hitung(99, 10);
+
+    }
+    
+}
